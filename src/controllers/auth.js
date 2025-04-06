6bcba29f3a170b0df2b85fcc980b495796d0a781
svichkar olga <syns41@gmail.com>
2025-04-06 13:35:35 +0300

    registration, login users done

---
+++ src/controllers/auth.js
@@ -0,0 +1,33 @@
+import { registerUser } from '../services/auth.js';
+
+export const registerUserController = async (req, res, next) => {
+  try {
+    const user = await registerUser(req.body);
+    res.status(201).json({
+      status: 201,
+      message: 'Successfully registered a user!',
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginUserController = async (req, res) => {
+  const session = await loginUser(req.body);
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+  res.json({
+    status: 200,
+    message: 'Successfully logged in a user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -4,7 +4,7 @@ const usersSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, minlength: 6, required: true },
+    password: { type: String, required: true },
   },
   { timestamps: true, versionKey: false },
 );
