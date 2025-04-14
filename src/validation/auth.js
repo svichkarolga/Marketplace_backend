48e96d538bc27561d736eb461f067b4fe422b0e6
svichkar olga <syns41@gmail.com>
2025-04-15 00:22:37 +0300

    changed length of the name

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,15 +1,15 @@
 import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+  name: Joi.string().min(1).max(150).required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
   phoneNumber: Joi.string()
     .pattern(/^[+]?[0-9]{10,15}$/)
-    .max(20)
+    .max(15)
     .messages({
       'string.base': 'PhoneNumber should start with "+"',
-      'string.max': 'PhoneNumber should have at most 20 characters',
+      'string.max': 'PhoneNumber should have at most 15 characters',
     }),
 });
 
