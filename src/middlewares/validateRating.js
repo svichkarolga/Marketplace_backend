f77f8caf0e386031c9af7eb736a45ee32034757b
Svichkar Olga <syns41@gmail.com>
2025-06-16 13:51:10 +0300

    swagger for review done

--- src/middlewares/validateRating.js
+++ src/middlewares/validateRating.js
@@ -6,31 +6,21 @@ export const validateRatingRequest = (req, res, next) => {
     const { sellerId } = req.params;
     const { rating } = req.body;
     const buyerId = req.user._id;
-
     if (!rating) {
       return next(createHttpError(400, 'Rating is required'));
     }
-
-    // 1. Проверка наличия rating в теле запроса
     if (rating === undefined) {
       throw createHttpError(400, 'Rating is required');
     }
-
-    // 2. Проверка формата sellerId
     if (!isValidObjectId(sellerId)) {
       throw createHttpError(400, 'Invalid seller ID format');
     }
-
-    // 3. Пользователь не может оценить себя
     if (buyerId.equals(sellerId)) {
       throw createHttpError(400, "You can't rate yourself");
     }
-
-    // 4. Проверка диапазона рейтинга
     if (typeof rating !== 'number' || rating < 1 || rating > 5) {
       throw createHttpError(400, 'Rating must be a number between 1 and 5');
     }
-
     req.ratingData = { sellerId, buyerId, rating };
     next();
   } catch (error) {
@@ -40,11 +30,9 @@ export const validateRatingRequest = (req, res, next) => {
 
 export const validateSellerId = (req, res, next) => {
   const { sellerId } = req.params;
-
   if (!isValidObjectId(sellerId)) {
     return next(createHttpError(400, 'Invalid seller ID'));
   }
-
   req.sellerId = sellerId;
   next();
 };
